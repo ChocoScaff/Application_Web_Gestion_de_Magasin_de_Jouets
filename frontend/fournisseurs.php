78b49e129014bf36eb41d94eb70cc1c289cdfdfd
ChocoScaff <anjul2@hotmail.fr>
2025-01-22 15:19:47 +0100

    add files in frontend

---
+++ frontend/fournisseurs.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Table</title>
+    <link rel='stylesheet' href='../assets/css/style.css'>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+    </style>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>Liste</title>
+</head>
+
+<body>
+<h1> liste des fournisseurs </h1>
+
+<nav>
+    <ul>
+        <li><a href='clients.php'>Client</a></li>
+        <li><a href='commandes.php'>Commande</a></li>
+        <li><a href='config.php'>config</a></li>
+        <li><a href='fournisseur.php'>Fournisseur</a></li>
+        <li><a href='jouet.php'>jouet</a></li>
+   
+    </ul>
+</nav>
+
+    <table border="1">
+        <tr>
+            <th>Nom</th>
+            <th>adresse</th>
+            <th>domaine</th>
+        </tr>
+
+
+
+</body>
